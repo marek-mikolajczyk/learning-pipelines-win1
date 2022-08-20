b2e588cc55b93b7249220c0be7583fa9158e3455
Marek Mikolajczyk <lalala@localhost.loca>
2022-08-20 21:30:32 +0200

    update

--- terraform-gitlab-project-configuration/project_variables.tf
+++ terraform-gitlab-project-configuration/project_variables.tf
@@ -4,3 +4,25 @@ resource "gitlab_project_variable" "artifactory_api" {
   value     = "http://docker:8081"
   protected = false
 }
+
+resource "gitlab_project_variable" "artifactory_user" {
+  project   = "2"
+  key       = "ARTIFACTORY_USER"
+  value     = "admin"
+  protected = false
+}
+
+
+resource "gitlab_project_variable" "artifactory_password" {
+  project   = "2"
+  key       = "ARTIFACTORY_PASSWORD"
+  value     = "Password123!"
+  protected = false
+}
+
+resource "gitlab_project_variable" "artifactory_repo_key" {
+  project   = "2"
+  key       = "ARTIFACTORY_REPO_KEY"
+  value     = "prj1-repo1"
+  protected = false
+}
